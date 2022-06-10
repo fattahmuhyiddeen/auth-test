a96f7b499af7aecc6dcbba15b39d9cc222fcdc9b
Fattah Muhyiddeen <fattahmuhyiddeen@gmail.com>
2022-06-10 09:00:54 +0800

    show auth status in root

--- controllers/general.js
+++ controllers/general.js
@@ -1,5 +1,19 @@
+const jwt = require('jsonwebtoken');
 const { version } = require('../package.json');
 
+
 exports.index = function (req, res) {
-  res.json({ version, date_time: new Date() });
+
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  // if (token == null) return res.sendStatus(401) // uncomment to block unauthenticated user
+
+  jwt.verify(token, process.env.PROJECT_JWT_SECRET, (err, user) => {
+    // if (err) return res.sendStatus(403) // uncomment to block unauthorized user
+
+    req.user = user
+    res.json({ version, date_time: new Date(), user });
+  });
+
 };
